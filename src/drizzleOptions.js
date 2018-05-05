9df5ac3d4c8ad8086d47473f4f5c927377ab4012
LayneHaber <layne.haber@gmail.com>
2018-05-04 20:20:45 -0700

    remove fallback web3 option

--- src/drizzleOptions.js
+++ src/drizzleOptions.js
@@ -3,10 +3,6 @@ import Moloch from './../build/contracts/Moloch.json'
 const drizzleOptions = {
   web3: {
     block: false,
-    fallback: {
-      type: 'ws',
-      url: 'ws://127.0.0.1:8545'
-    }
   },
   contracts: [
     Moloch
