03867dd4fe9d4c9d839adb5d5fe41f08a69cd5d1
LayneHaber <layne.haber@gmail.com>
2018-05-08 14:49:44 -0700

    make message dismissable

--- src/layouts/messages/MessageList.js
+++ src/layouts/messages/MessageList.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react'
 import { Container, Message, Icon } from 'semantic-ui-react'
 
 class MessageList extends Component {
+  state = {
+    visible: true
+  }
+
+  handleDismiss = () => {
+    this.setState({ visible: false })
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (!nextProps || nextProps == this.props) {
+      // no new props
+      return
+    } else {
+      this.setState({ visible: true})
+    }
+  }
+
   createMessage = (msg, index) => {
     return (
       <div key={index}>
         <Message
+          hidden={!this.state.visible}
           icon
           positive={msg.sentiment === 'positive'}
           negative={msg.sentiment === 'negative'}
           info={msg.sentiment === 'info'}
+          onDismiss={this.handleDismiss}
         >
-          <Icon className='close icon' />
           <Icon className={msg.icon} />
           <Message.Content>
             <Message.Header content={msg.title} />
