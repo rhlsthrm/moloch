bf792596c5247174fd45c184d5011fd83b80f453
LayneHaber <layne.haber@gmail.com>
2018-05-09 19:56:32 -0700

    add other contracts to drizzle options and town hall lib events

--- src/drizzleOptions.js
+++ src/drizzleOptions.js
@@ -1,4 +1,9 @@
 import Moloch from './../build/contracts/Moloch.json'
+import GuildBank from './../build/contracts/GuildBank.json'
+import LootToken from './../build/contracts/LootToken.json'
+import TownHallLib from './../build/contracts/TownHallLib.json'
+import VotingLib from './../build/contracts/VotingLib.json'
+import VotingShares from './../build/contracts/VotingShares.json'
 
 const drizzleOptions = {
   web3: {
@@ -6,12 +11,23 @@ const drizzleOptions = {
   },
   contracts: [
     Moloch,
+    GuildBank,
+    LootToken,
+    TownHallLib,
+    VotingLib,
+    VotingShares
   ],
   events: {
     Moloch: [ 
       'MemberAccepted',
       'MemberExit'
-    ]
+    ],
+    TownHallLib: [
+      'ProposalCreated',
+      'ProposalVotingStarted',
+      'ProposalGracePeriodStarted',
+      'ProposalCompleted'
+    ],
   }
 }
 
